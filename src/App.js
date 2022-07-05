0b0e89e4d19213e7d6da2d44d8898c5ac3e70715
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2022-07-05 15:53:46 -0700

    repushing because i accidentally deleted part of App.js :)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-
+import Square from './components/Square.js'
 class App extends Component {
   constructor(props) {
     super(props)
@@ -8,11 +8,29 @@ class App extends Component {
       board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
     }
   }
+  handleGamePlay = (index) => {
+    // alert(index)
+    // destructure state so that we can access an instance of board
+    const { board } = this.state
+    // set selected board item (found by index) to emoji
+    board[index] = "🌴"
+    this.setState({ board: board})
+  }
 
   render() {
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className="gameboard">
+        {this.state.board.map((value, index) => {
+          return <Square
+            key={index}
+            index={index}
+            value={value}
+            handleClick={this.handleGamePlay}
+          />
+        })}
+        </div>
       </>
     )
   }
